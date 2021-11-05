3fcea25564060ba9024fae5e43f29903c5ef8f41
boryand <boryand>
2021-11-05 16:16:21 +0100

    Step1done

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 app.use(express.json());
-const port = process.env.PORT || 3001;
+const cors = require('cors')
 let data = require("./data.json");
+const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  console.log(data);
+app.use(cors())
+// app.get("/pokemon", (req, res) => {
+//     req.send
+// });
+
+app.get("/pokemon", (req, res) => {
+  res.send(data);
+});
+
+app.get("/pokemon/:id", (req, res) => {
+  const { id } = req.params;
+// everything that comes from req.params is a string
+
+  const result = data.filter((entry) => entry.id === Number(id));
+  res.send(result);
 });
 
 app.listen(port, () => {
